ad531d25f417d901e26069497ab474307317eafc
Ricardo Longa <ricardo.longa@gmail.com>
2013-10-29 23:40:13 -0200

    Incluindo a integração com o Linkedin.

--- src/main/java/org/gujavasc/opennetworking/android/application/LoginActivity.java
+++ src/main/java/org/gujavasc/opennetworking/android/application/LoginActivity.java
@@ -1,33 +1,67 @@
 package org.gujavasc.opennetworking.android.application;
 
+import org.brickred.socialauth.android.DialogListener;
+import org.brickred.socialauth.android.SocialAuthAdapter;
+import org.brickred.socialauth.android.SocialAuthAdapter.Provider;
+import org.brickred.socialauth.android.SocialAuthError;
 import org.gujavasc.opennetworking.android.R;
 
 import android.app.Activity;
-import android.content.ComponentName;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 
 public class LoginActivity extends Activity {
 	
+    // SocialAuth Component
+    SocialAuthAdapter adapter;
+    boolean status;
+    
 	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
         
+        adapter = new SocialAuthAdapter(new ResponseListener());
+
+        try {
+            adapter.addConfig(Provider.LINKEDIN, "bnv8x51yjbdo", "y0nvGzOYjw1KyKdV", null);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         Button botao = (Button) findViewById(R.id.login_by_linkedin);  
+        
         botao.setOnClickListener(new OnClickListener() {
             @Override  
-            public void onClick(View v) {  
-            	test();
+            public void onClick(View v) {
+                adapter.authorize(LoginActivity.this, Provider.LINKEDIN);
             }  
-        });  
+        });
 	}
-	
-	private void test() {
-		startActivity(new Intent(LoginActivity.this, MainActivity.class));
-	}
-	
+
+    private final class ResponseListener implements DialogListener {
+        @Override
+        public void onComplete(Bundle values) {
+            startActivity(new Intent(LoginActivity.this, MainActivity.class));
+        }
+
+        @Override
+        public void onError(SocialAuthError error) {
+            Log.d("Share-Bar", error.getMessage());
+        }
+
+        @Override
+        public void onCancel() {
+            Log.d("Share-Bar", "Authentication Cancelled");
+        }
+
+        @Override
+        public void onBack() {
+            Log.d("Share-Bar", "Dialog Closed by pressing Back Key");
+        }
+    }
 }
